1f7fd2d55ceea961a5e89e75297956db608c16f8
anit3 <anit@nimapinfotech.com>
2022-07-01 18:17:04 +0530

    clean changes

---
+++ src/components/CleanMumbai.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const CleanMumbai = () => {
+  return (
+    <div className='clean'>
+      <div className='clean__head'>
+        <p>Mobile <br/>Application &nbsp;----</p>
+        <h1 className='clean__head--title'>Clean Mumbai Application</h1>
+        <p>Clean Mumbai is a mobile app that 
+          assists in keeping the city clean. 
+          It promotes the convergence of BMC and Mumbai residents into a single platform. It allows citizens the ease of connecting with BMC workers just through one app. No more searching phone numbers from numerous people.
+        </p>
+        <div>
+          <p>Year<br/><br/>2021</p>
+          <p>Role<br/><br/>2UI/UX</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CleanMumbai
